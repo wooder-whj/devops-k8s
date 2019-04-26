224a354ecbe6a81f8766b6c39cb9c25bda4c1be6
unknown <1153657192@qq.com>
2019-04-26 19:22:59 +0800

    update readme & devops.sh

--- shell/devops.sh
+++ shell/devops.sh
@@ -17,10 +17,10 @@ while true; do
        count=`grep -c $context:$tag jar-registry.txt`
        ##if [[ -z $jarRegi ]]; then
        if [ $count == '0' ]; then
-          echo "$context:$tag" > jar-registry.txt
-          for node in `kubectl get nodes|awk -F ' ' 'NR>1 { print $1}'`; do
+          echo "$context:$tag" >> jar-registry.txt
+          for node in `kubectl get nodes|awk -F ' ' 'NR>1 { print $1 }'`; do
             if [ $host != $node ]; then
-              echo $node":"$context:$tag >> node-registry.txt
+              echo $node:$context:$tag >> node-registry.txt
             fi
           done
        fi
@@ -31,33 +31,38 @@ while true; do
        if [ $? == 0 ]; then
          context=`echo $regi|awk -F':' '{ print $2 }'`
          tag=`echo $regi|awk -F':' '{ print $3 }'`
-         cat > Dockerfile << EOF
+         test -e $context-$tag*.jar
+         if [ $? != 0 ]; then
+            sed -i "/$context:$tag/d" node-registry.txt
+            sed -i "/$context:$tag/d" jar-registry.txt
+         else
+           cat > Dockerfile << EOF
 FROM openjdk
 RUN mkdir -p /$context
 ADD ["./$jar","/$context/"]
 CMD java -jar /$context/$jar
 EOF
-##       build image
-         docker image inspect $context:$tag > /dev/null 2>&1
-         if [ $? == 0 ]; then
-            docker rmi $context:$tag
-         fi
-         docker build -t $context:$tag ./
-         docker save $context:$tag > /tmp/$context.tar.gz 
-##       build images for all joined nodes
-       # for node in `kubectl get nodes|awk -F ' ' 'NR>1 { print $1}'`; do
-         if [ $host != $node ]; then
-           scp -i ~/.ssh/id_rsa /tmp/$context.tar.gz root@$node:/tmp
+##        ##build image
+           docker image inspect $context:$tag > /dev/null 2>&1
            if [ $? == 0 ]; then
-           ssh -i ~/.ssh/id_rsa root@$node > ./logs/ssh.log << EOF
+              docker rmi $context:$tag
+           fi
+           docker build -t $context:$tag ./
+           docker save $context:$tag > /tmp/$context.tar.gz 
+##       ##build images for all joined nodes
+       # for node in `kubectl get nodes|awk -F ' ' 'NR>1 { print $1}'`; do
+           if [ $host != $node ]; then
+             scp -i ~/.ssh/id_rsa /tmp/$context.tar.gz root@$node:/tmp
+             if [ $? == 0 ]; then
+             ssh -i ~/.ssh/id_rsa root@$node > ./logs/ssh.log << EOF
 docker rmi -f $context:$tag
 docker load < /tmp/$context.tar.gz
 exit
 EOF
-              if [ $? == 0 ]; then
-                 sed -i "/$node/d" node-registry.txt
+                if [ $? == 0 ]; then
+                   sed -i "/$node/d" node-registry.txt
+                fi
               fi
-            fi
 ##           if [ $? == 0 ]; then
 ##            docker image inspect $context:$tag > /dev/null 2>&1
 ##            if [ $? == 0 ]; then
@@ -72,11 +77,11 @@ EOF
 ##           else
 ##             echo "cannot login $node"
 ##           fi
-         fi   
+           fi   
        # done
-       docker rmi $context:$tag
-       deploy=`echo $context"_deployment".yaml`
-       cat > ./yaml/$deploy << EOF
+         docker rmi $context:$tag
+         deploy=`echo $context"_deployment".yaml`
+         cat > ./yaml/$deploy << EOF
 apiVersion: extensions/v1beta1
 kind: Deployment
 metadata:
@@ -107,8 +112,8 @@ spec:
         - containerPort: 8001
           hostPort: 8001
 EOF
-       svc=`echo $context"_svc".yaml`
-       cat > ./yaml/$svc << EOF
+         svc=`echo $context"_svc".yaml`
+         cat > ./yaml/$svc << EOF
 apiVersion: v1
 kind: Service
 metadata:
@@ -123,16 +128,17 @@ spec:
     targetPort: 8001
     nodePort: 30801
 EOF
-       kubectl apply -f ./yaml/$deploy
-       kubectl apply -f ./yaml/$svc
+         kubectl apply -f ./yaml/$deploy
+         kubectl apply -f ./yaml/$svc
      ## success=`sed -n "/$context:$tag/p" node-registry.txt`
      ## if [[ -z $success ]]; then
-       success=`grep -c $context:$tag node-registry.txt`
-       if [ $success == '0' ]; then
-         sed -i "/$context:$tag/d" jar-registry.txt
-         mv $jar ./backup
+         success=`grep -c $context:$tag node-registry.txt`
+         if [ $success == '0' ]; then
+           sed -i "/$context:$tag/d" jar-registry.txt
+           mv $jar ./backup
+         fi
        fi
-     fi 
+      fi 
     done
   else
     rm -f *-registry.txt
